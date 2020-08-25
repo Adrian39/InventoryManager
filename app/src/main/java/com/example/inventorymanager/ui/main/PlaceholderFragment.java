140953415568c988cb9be5a265b22c545a4b1ae2
Adrian <adrian.lovi.39@gmail.com>
2020-08-25 14:22:39 -0500

    Added dependencies to prepare for communication with database

--- app/src/main/java/com/example/inventorymanager/ui/main/PlaceholderFragment.java
+++ app/src/main/java/com/example/inventorymanager/ui/main/PlaceholderFragment.java
@@ -1,6 +1,7 @@
 package com.example.inventorymanager.ui.main;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,24 +22,35 @@ import com.example.inventorymanager.R;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.security.PrivilegedAction;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class PlaceholderFragment extends Fragment {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
-
     private PageViewModel pageViewModel;
-
-
-
-
-
     int amount = 0;
+    //GUI VARIABLES
     private ImageButton minusButton;
     private ImageButton plusButton;
     private EditText amountEditText;
+    private EditText codeEditText;
+    private TextView productName;
 
+    //INTERNET CONNECTION VARIABLES
+    private static final String IP = "192.168.0.12";
+    private static final String PORT = "1433";
+    private static String mClass = "net.sourceforge.jtds.jdbc.Driver";
+    private static final String DATABASE = "adCOMER2018";
+    private static String mUserName = "test";
+    private static String mPass = "test";
+    private static String mURL = "jdbc:jtds:sqlserver://" + IP + ":" + PORT + "/" + DATABASE;
+    private Connection mConnection = null;
 
     public static PlaceholderFragment newInstance(int index) {
         PlaceholderFragment fragment = new PlaceholderFragment();
@@ -51,6 +63,7 @@ public class PlaceholderFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         pageViewModel = ViewModelProviders.of(this).get(PageViewModel.class);
         int index = 1;
 
@@ -60,6 +73,21 @@ public class PlaceholderFragment extends Fragment {
             index = getArguments().getInt(ARG_SECTION_NUMBER);
         }
         pageViewModel.setIndex(index);
+
+        //DATABASE CONNECTION
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        try {
+            Class.forName(mClass);
+            mConnection = DriverManager.getConnection(mURL, mUserName, mPass);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
@@ -110,6 +138,10 @@ public class PlaceholderFragment extends Fragment {
         }
         amount--;
         amountEditText.setText(String.valueOf(amount));
+    }
+
+    private void searchItemByCode(View view){
+        
     }
 
 }
